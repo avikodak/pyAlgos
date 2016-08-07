bbf44946da92671b64ebfbd1f58cfa91e97e9100
Avinash Kodakandla <avikodak@gmail.com>
2016-08-07 10:06:58 +0530

    first commit python

---
+++ src/main/com/avikodak/sites/geeksforgeeks/practice/basic/findtheremainder.py
@@ -0,0 +1,15 @@
+'''
+File Name                   : findoddoccurence.h
+Created on                  : Aug 5, 2016
+Author                      : avikodak
+Testing Status              : Tested
+URL                         : http://www.practice.geeksforgeeks.org/problem-page.php?pid=843
+'''
+
+'''
+Tested
+'''
+testCases = int(input())
+for counter in range(1, testCases + 1):
+    num = int(input())
+    print(num % 7)
